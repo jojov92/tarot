ac44a7e75d3c03e5eb36615bb167c6ad424eb950
Johti Vashisht <johti@Johtis-MacBook-Pro.local>
2018-10-13 19:46:27 +0100

    description

--- tarot.js
+++ tarot.js
@@ -1,4 +1,19 @@
-const tarotCard = [4,5,6,7];
+const tarotCardDesc= [ { 
+                    src: 'card-0.jpg',
+                    desc: 'This is the Queen of Cups tarot card'
+                }, 
+                {src: 'card-1.jpg',
+                    desc: 'This is the Fool tarot card'
+            }, 
+
+                {src: 'card-2.jpg',
+                    desc: 'This is the Star tarot card'
+                }, 
+
+                {src: 'card-3.jpg',
+                    desc: 'This is the Hanged Man tarot card'
+                } ];
+  
 /* console.log(tarotCard[3]);
 
 //creates a random number between 0 and 1 
@@ -6,7 +21,7 @@ const index = Math.random();
 console.log(index);*/
 
 
-function pickRandomCard() {
+function pickRandomCard1() {
     const index = Math.random();
     const max = 3;
     const min = 0;
@@ -14,23 +29,31 @@ function pickRandomCard() {
 return cardNumber;
 }
 
-console.log("randomCard", pickRandomCard());
+//console.log("randomCard", pickRandomCard1());
 
 
 function drawACard() {
-console.log(pickRandomCard());
+console.log(pickRandomCard1());
+const pickRandomCard = pickRandomCard1();
+
 
 const img = document.querySelector(".picked-card img");
-img.src = `card-${pickRandomCard()}.jpg`;
+//calling random card 
+img.src = `card-${pickRandomCard}.jpg`;
 const container = document.querySelector(".picked-card");
 //adding something to the class list
 container.classList.add("show");
 console.log(img.src);
+console.log(tarotCardDesc[pickRandomCard]);
+
+const hide = document.querySelector(".card-description");
+        hide.classList.remove("hide");
 
 
-//const el = document.querySelector(".picked-card")
-//el.innerHTML = pickRandomCard();
-//console.log(el);
+const cardDescription = document.querySelector(".card-description")
+//it will pick a random card but also show the accompanying description
+cardDescription.innerHTML = tarotCardDesc[pickRandomCard].desc;
+console.log(cardDescription);
 
 }
 
@@ -38,6 +61,9 @@ function shuffleCard() {
 
     const container = document.querySelector(".picked-card");
         container.classList.remove("show");
+       
+        const hide = document.querySelector(".card-description");
+        hide.classList.add("hide");
 }
 // this is the max and min numbers we can call
 //const max = 77;
